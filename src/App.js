eb2fe7d6373ecb54e4d27b38bde01280eb941ef4
soohyeon.won <main.async@kakao.com>
2023-03-27 22:13:01 +0900

    button onClick={func} 이벤트 생성

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import Todo from './components/Todo'
 function App() {
   return (
     <div>
-      <h1>My Todos</h1>
-      <Todo test='Learn React'/> // 셀프 클로징 테그 
-      <Todo test='Learn Swift'/>
+    <h1>My Todos</h1>
+      <Todo text='Learn React'/> {/* 셀프 클로징 테그  */} 
+      <Todo text='Learn Swift'/>
     </div>
   );
 }
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,15 +1,19 @@
 function Todo(props) {
     // 함수 이름은 대문자로 설정
     // html코드와 분리되어야함
+
+    function deleteHandler() {
+      console.log('click!');
+      console.log(props.text);
+    }
     return ( //JSX CODE
-        <div>
-          <h1>My Todos</h1>
           <div className='card'>
-            <h2>{props.text}</h2> // javaScript 코드로 실행됨
-            <button className='btn'>Delete</button>
+            <h2>{props.text}</h2> {/* javaScript 코드로 실행됨 */} 
+            <div className='actions'>
+            <button className='btn' onClick={deleteHandler}>Delete</button>
+            </div>
           </div>
-        </div>
-      )
+      );
 }
 
 export default Todo;
