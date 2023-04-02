6c6cdf07b2646caff9bc757a59aaac116d3d6dc3
soohyeon.won <main.async@kakao.com>
2023-04-02 21:41:05 +0900

    링크&라우터 추가하기

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
+import { Route, Switch } from 'react-router-dom/cjs/react-router-dom.min';
 import './App.css';
 import Todo from './components/Todo'
+import NewMeetupPage from './pages/NewMeetup';
 
 function App() {
   return (
     <div>
-    <h1>My Todos</h1>
-      <Todo text='Learn React'/> {/* 셀프 클로징 테그  */} 
-      <Todo text='Learn Swift'/>
+      <Switch>
+      <Route path='/' exact> {/* exat = {true}와 같음*/}
+        <NewMeetupPage />
+      </Route>
+      <Route path='/todo'>
+        <h1>My Todos</h1>
+        <Todo text='Learn React'/> {/* 셀프 클로징 테그  */} 
+        <Todo text='Learn Swift'/>
+      </Route>
+    </Switch>
     </div>
   );
 }
